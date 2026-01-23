0e2a6b330301648407ac983c226dd74202acaf2d
Frank <205842738+frankangel-dev@users.noreply.github.com>
2026-01-23 02:47:50 -0800

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -24,3 +24,90 @@ if (skillsSection) {
         }
     }
 }
+
+// Message Form
+const messageForm = document.querySelector('form[name="leave_message"]');
+const messageSection = document.getElementById('messages');
+const messageList = messageSection.querySelector('ul');
+
+if (messageList.children.length === 0) {
+    messageSection.style.display = 'none';
+}
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = e.target.usersName.value;
+    const email = e.target.usersEmail.value;
+    const message = e.target.usersMessage.value;
+    const emailSymbol = '\u{1F4E7}';
+
+    messageSection.style.display = 'block';
+
+    const newMessage = document.createElement('li');
+    const anchor = document.createElement('a');
+    anchor.href = `mailto:${email}`;
+    anchor.textContent = `${name.toUpperCase()} ${emailSymbol}`;
+
+    const messageText = document.createElement('span');
+    messageText.textContent = message;
+
+    newMessage.appendChild(anchor);
+    newMessage.appendChild(messageText);
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('btn-remove');
+    removeButton.textContent = 'Remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', () => {
+        newMessage.remove();
+
+        if (messageList.children.length === 0) {
+            messageSection.style.display = 'none';
+        }
+    });
+
+    const editButton = document.createElement('button');
+    editButton.classList.add('btn-edit');
+    editButton.textContent = 'Edit';
+    editButton.type = 'button';
+    editButton.dataset.mode = 'Edit';
+
+    editButton.addEventListener('click', e => {
+        const button = e.target;
+        const messageSpan = newMessage.querySelector('span');
+
+        if (button.dataset.mode === 'Edit') {
+            const inputField = document.createElement('input');
+            inputField.type = 'text';
+            inputField.value = messageSpan.textContent;
+            newMessage.insertBefore(inputField, messageSpan);
+            messageSpan.remove();
+            button.textContent = 'Save';
+            editButton.dataset.mode = 'Save';
+        } else if (button.dataset.mode === 'Save') {
+            const input = newMessage.querySelector('input');
+            const text = input.value.trim();
+            if (text !== "") {
+                const newSpan = document.createElement('span');
+                newSpan.textContent = text;
+                newMessage.insertBefore(newSpan, input);
+                input.remove();
+                button.textContent = 'Edit';
+                button.dataset.mode = 'Edit';
+            } else {
+                input.style.borderColor = 'red';
+            }
+        }
+    });
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('btn-container');
+    buttonContainer.appendChild(removeButton);
+    buttonContainer.appendChild(editButton);
+
+    newMessage.appendChild(buttonContainer);
+
+    messageList.appendChild(newMessage);
+    
+    e.target.reset();
+});
